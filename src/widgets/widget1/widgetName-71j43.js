da61faa8df6596bb67cb78b6859409fadb4871bc
Uri <=>
2022-03-09 06:49:42 +0200

    new changes

---
+++ src/widgets/widget1/widgetName-71j43.js
@@ -0,0 +1,13 @@
+// For Velo API Reference documentation visit http://wix.to/94BuAAs
+// To learn about widget code visit https://www.wix.com/app-builder/home/resources
+
+$w.onReady(function () {
+	// Initialize your widget here. If your widget has properties, this is a good place to read their values and initialize the widget accordingly.
+	
+});
+
+$widget.onPropsChanged((oldProps, newProps) => {
+	// If your widget has properties, onPropsChanged is where you should handle changes to their values.
+	// Property values can change at runtime by code written on the site, or when you preview your widget here in the App Builder.
+	
+});
